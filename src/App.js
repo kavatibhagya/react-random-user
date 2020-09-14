f7c962222ca2121fa2374d487b8e3e17866114b3
Bhagya Kavati <56948344+kavatibhagya@users.noreply.github.com>
2020-09-13 20:31:16 -0700

    Added gender filter

--- src/App.js
+++ src/App.js
@@ -3,23 +3,63 @@ import "./styles.css";
 import axios from "axios";
 
 export default function App() {
+  const [gender, setGender] = useState("");
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await axios("https://randomuser.me/api/?results=20");
+  async function fetchData(param) {
+    let url = "https://randomuser.me/api/?results=20";
 
-      if (response && response.data) {
-        setUsers([...response.data.results]);
-      }
+    if (param) {
+      url = `${url}&gender=${param}`;
     }
+    const response = await axios(url);
 
+    if (response && response.data) {
+      setUsers([...response.data.results]);
+    }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
+
+  const filterusers = (e) => {
+    e.preventDefault();
+
+    fetchData(gender);
+  };
+
+  const genderChange = (e) => {
+    setGender(e.target.value);
+  };
 
   return (
     <div className="App">
       <h1>Random users</h1>
+      <h4>Filter by gender</h4>
+      <form onSubmit={filterusers}>
+        <p>
+          <label>Male</label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={gender === "male"}
+            onChange={genderChange}
+          />
+        </p>
+        <p>
+          <label>Female</label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={gender === "female"}
+            onChange={genderChange}
+          />
+        </p>
+        <button type="submit">Filter</button>
+      </form>
       <ul>
         {users.map((user) => (
           <li key={user.login.uuid}>
