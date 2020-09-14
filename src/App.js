645b1ed2bdff09d214e0f65c2b5597ad6833616a
Bhagya Kavati <56948344+kavatibhagya@users.noreply.github.com>
2020-09-13 20:16:06 -0700

    Added api and template logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import axios from "axios";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios("https://randomuser.me/api/?results=20");
+
+      if (response && response.data) {
+        setUsers([...response.data.results]);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Random users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login.uuid}>
+            <img
+              src={user.picture.medium}
+              width="100px"
+              height="100px"
+              alt="profile"
+            ></img>
+            <div>
+              {user.name.title} {user.name.first} {user.name.last}
+            </div>
+            <div>{user.gender}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
